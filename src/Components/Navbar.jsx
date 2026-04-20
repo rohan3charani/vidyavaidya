c1e69b66574f6dd6111fef3fb10e38dbd51d76f9
irohan <irohansha@gmail.com>
2026-04-20 12:04:44 +0530

    A

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -144,7 +144,7 @@ export default function Navbar() {
             }}
             className="vv-nav-link vv-join-community"
           >
-            Be Part of Us
+            Be A Part of Us
           </button>
           <button
             onClick={() => {
